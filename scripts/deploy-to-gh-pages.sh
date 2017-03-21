1468ee933a2d3eede46881a93c4fb6c773902131
Vilsol <vilsol2000@gmail.com>
2017-03-21 09:40:01 +0000

    Add travis deployment

---
+++ scripts/deploy-to-gh-pages.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -o errexit
+
+if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "master" ]; then exit 0; fi
+
+rm -rf public
+mkdir public
+
+git config --global user.email "nobody@nobody.org"
+git config --global user.name "Travis CI"
+
+npm install raml2html
+
+raml2html API/raml/v1/index.raml > public/index.html
+
+cd public
+git init
+git add .
+git commit -m "Deploy to Github Pages"
+git push --force --quiet "https://${GITHUB_TOKEN}@$github.com/${GITHUB_REPO}.git" master:gh-pages > /dev/null 2>&1
